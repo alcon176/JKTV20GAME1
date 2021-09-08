3f5b7f5c7a02bfef28e152135c076f6d60fe8a32
pupil <pupil@ivkhk.ee>
2021-09-08 11:23:26 +0300

    второй коммит

--- src/game/Game.java
+++ src/game/Game.java
@@ -5,6 +5,9 @@
  */
 package game;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author pupil
@@ -15,7 +18,17 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int number=random.nextInt(9-0+1)+0;
+        System.out.println("Программа запомнила число от 0 до 9, угадай");
+        int userNumber = scanner.nextInt();
+        if(number == userNumber){
+            System.out.println("Ура, Ты Выйграл");      
+        }else{
+            System.out.println(":(, неправильно. Задуманно число:"+number);
+        }
+                
     }
     
 }
